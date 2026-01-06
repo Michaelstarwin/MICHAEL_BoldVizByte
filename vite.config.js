fd8fcf76e97aa03f103ec7cbb5785d9accbff381
Michaelstarwin <michealstarwin323@gmail.com>
2026-01-06 14:28:16 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
     host: true, // Listen on all addresses (0.0.0.0)
     port: 4173,
     allowedHosts: [
-      'boldvizbyte.onrender.com' // Whitelist Render domain
+      'boldvizbyte.onrender.com', // Whitelist Render domain
+      'michael-boldvizbyte.onrender.com'
     ]
   },
   server: {
